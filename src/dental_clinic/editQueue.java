e7dfdc0576e2d524394b367469192a128bb4dae7
Windows10 <Windows10@DESKTOP-K06BB4S.local>
2024-11-04 00:30:01 +0800

    hi

--- src/dental_clinic/editQueue.java
+++ src/dental_clinic/editQueue.java
@@ -10,16 +10,42 @@ package dental_clinic;
  * @author Ana Marie Lim
  */
 
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 public class editQueue extends javax.swing.JFrame {
 
     /** Creates new form fullPatientRecord */
     public editQueue() {
         initComponents();
         this.setLocationRelativeTo(null);
+        Connect();
 
     }
+    
+    
+    Statement st;
+    Connection con;
+    PreparedStatement pst;
 
+      public void Connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dental", "root", "padabaKO21");
+            System.out.println("Connected!");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(patientInformationFrame.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error 1!");
+        } catch (SQLException ex) {
+            Logger.getLogger(patientInformationFrame.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error 2!");
+        }
+
+    }
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -35,11 +61,18 @@ public class editQueue extends javax.swing.JFrame {
         removeButton = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
         backButton = new javax.swing.JButton();
         background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(1170, 710));
+        addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent evt) {
+                formComponentShown(evt);
+            }
+        });
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setMaximumSize(new java.awt.Dimension(2147483647, 2147483647));
@@ -69,6 +102,11 @@ public class editQueue extends javax.swing.JFrame {
 
         removeButton.setContentAreaFilled(false);
         removeButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        removeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                removeButtonActionPerformed(evt);
+            }
+        });
         jPanel1.add(removeButton);
         removeButton.setBounds(950, 73, 150, 50);
 
@@ -101,7 +139,35 @@ public class editQueue extends javax.swing.JFrame {
         }
 
         jPanel1.add(jScrollPane1);
-        jScrollPane1.setBounds(70, 172, 1030, 460);
+        jScrollPane1.setBounds(50, 160, 550, 470);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Patient Name", "Queue Number", "Category", "Service Availed", "Priority"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, true
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(jTable2);
+        if (jTable2.getColumnModel().getColumnCount() > 0) {
+            jTable2.getColumnModel().getColumn(0).setResizable(false);
+            jTable2.getColumnModel().getColumn(1).setResizable(false);
+            jTable2.getColumnModel().getColumn(2).setResizable(false);
+            jTable2.getColumnModel().getColumn(3).setResizable(false);
+            jTable2.getColumnModel().getColumn(4).setResizable(false);
+        }
+
+        jPanel1.add(jScrollPane2);
+        jScrollPane2.setBounds(650, 160, 510, 470);
 
         backButton.setContentAreaFilled(false);
         backButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -113,7 +179,7 @@ public class editQueue extends javax.swing.JFrame {
         jPanel1.add(backButton);
         backButton.setBounds(70, 653, 40, 30);
 
-        background.setIcon(new javax.swing.ImageIcon("C:\\Users\\Ana Marie Lim\\Documents\\Dental\\2\\Updated\\Edit Queue.png")); // NOI18N
+        background.setIcon(new javax.swing.ImageIcon("C:\\Users\\Windows10\\Downloads\\New Edit Queue.png")); // NOI18N
         background.setText("asd");
         background.setMaximumSize(new java.awt.Dimension(1170, 710));
         background.setMinimumSize(new java.awt.Dimension(1170, 710));
@@ -133,12 +199,49 @@ public class editQueue extends javax.swing.JFrame {
     }//GEN-LAST:event_backButtonActionPerformed
 
     private void editButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editButtonActionPerformed
-        
+    
+                try {
+              st = con.createStatement();
+              st.executeUpdate("DELETE FROM patientrecords");
+              JOptionPane.showMessageDialog(null, "All records deleted successfully.");
+              this.setVisible(false);
+              new editQueue().setVisible(true);
+                  }
+           
+          
+            catch(SQLException ed){
+            JOptionPane.showMessageDialog(null,"ERROR");
+            }
     }//GEN-LAST:event_editButtonActionPerformed
 
     private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
+        new dentalServicesFrame().setVisible(true);
+
     }//GEN-LAST:event_addButtonActionPerformed
+
+    private void removeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removeButtonActionPerformed
+
+
+    }//GEN-LAST:event_removeButtonActionPerformed
+
+    private void formComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_formComponentShown
+          try{  
+            pst = con.prepareStatement("SELECT *FROM patientrecords");
+            ResultSet rs = pst.executeQuery();
+
+            DefaultTableModel model = (DefaultTableModel)jTable1.getModel();
+            model.setRowCount(0);
+
+            while(rs.next()){
+                model.addRow(new Object[]{rs.getInt("patient_pk"), rs.getString("patientName"),rs.getString("patientBod"),rs.getString("patientContact"), rs.getString("patientServices")});
+            }
+          }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }//GEN-LAST:event_formComponentShown
 
     /**
      * @param args the command line arguments
@@ -185,7 +288,9 @@ public class editQueue extends javax.swing.JFrame {
     private javax.swing.JButton editButton;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
     private javax.swing.JButton removeButton;
     // End of variables declaration//GEN-END:variables
 
